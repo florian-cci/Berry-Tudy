6e114c4b324960714f726511b60a4d66be14ceb2
paki leonel <pakileonel1@gmail.com>
2022-09-23 17:38:01 +0100

    push add

--- config/systempay.php
+++ config/systempay.php
@@ -2,11 +2,17 @@
 
 return [
     'default' => [
-        'site_id' => 'YOUR_SITE_ID',
-        'key'     => env('SYSTEMPAY_SITE_KEY', 'YOUR_KEY'),
-        'env'     => env('SYSTEMPAY_ENV', 'PRODUCTION'),
+        'site_id' => env('SITE_ID'),
+        'key'     => env('SYSTEMPAY_SITE_KEY'),
+        'env'     => env('SYSTEMPAY_ENV'),
+        'algo'    => 'sha1',
         'params'  => [
-            'currency' => '978'
+            'vads_page_action' => 'PAYMENT',
+            'vads_action_mode' => 'INTERACTIVE',
+            'vads_payment_config' => 'SINGLE',
+            'vads_page_action' => 'PAYMENT',
+            'vads_version' => 'V2',
+            'vads_currency' => '978'
         ]
     ],
 ];
