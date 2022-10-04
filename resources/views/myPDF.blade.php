9ba044e1eb695680f611c6a1c473c9641f6563ee
paki leonel <pakileonel1@gmail.com>
2022-10-04 17:01:37 +0100

    push add

--- resources/views/myPDF.blade.php
+++ resources/views/myPDF.blade.php
@@ -7,9 +7,8 @@
     <h1 align="center">{{ $title }}</h1>
     <table align="center" border="0" cellpadding="0" cellspacing="0" style="width:100%">
         @foreach ($info as $item=>$value)
-            @if ($item!="Inscription_User_ID")
-                <tr>
-                    <td>{{  ucfirst(str_replace(['_',''],[' ',' '],$item) )}}</td>
+            @if ($item!="Inscription_User_ID" && $item!="created_at" && $item!="updated_at" && $item!="deleted_at")
+                    <td>{{  ucfirst(str_replace(['_','Inscription'],[' ',' '],$item) )}}</td>
                     <td>{{ $value }}</td>
                 </tr>
             @endif
