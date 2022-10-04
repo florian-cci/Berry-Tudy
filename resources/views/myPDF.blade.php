a4bd952f25fc45e863904a48d660b5056e66456b
paki leonel <pakileonel1@gmail.com>
2022-10-04 17:07:46 +0100

    push add

--- resources/views/myPDF.blade.php
+++ resources/views/myPDF.blade.php
@@ -5,7 +5,7 @@
 </head>
 <body>
     <h1 align="center">{{ $title }}</h1>
-    <table align="center" border="0" cellpadding="0" cellspacing="0" style="width:100%">
+    <table align="center" style="width:100%">
         @foreach ($info as $item=>$value)
             @if ($item!="Inscription_User_ID" && $item!="created_at" && $item!="updated_at" && $item!="deleted_at")
                 <tr>
